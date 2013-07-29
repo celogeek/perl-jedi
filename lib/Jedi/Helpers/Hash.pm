b5ad26831ee1672d52e38604ae58732a363c25e1
celogeek <me@celogeek.com>
2013-07-29 20:11:19 +0200

    add pod to Jedi::Helpers

--- lib/Jedi/Helpers/Hash.pm
+++ lib/Jedi/Helpers/Hash.pm
@@ -1,8 +1,22 @@
 package Jedi::Helpers::Hash;
+
+# ABSTRACT: Jedi Helpers for Hash
+
 use strict;
 use warnings;
+
+# VERSION
+
 use Import::Into;
 use Module::Runtime qw/use_module/;
+
+=method import
+
+Equivalent in your module to :
+
+  use autobox HASH => Jedi::Helpers::Hash
+
+=cut
 
 sub import {
 	my ($class) = @_;
@@ -11,6 +25,26 @@ sub import {
 	return;
 }
 
+=method to_arrayref
+
+Transform an headers form into an arrayref
+
+Ex :
+
+	{
+		'X-Test' => [ "AAA" ],
+		'Set-Cookie' => ["T1=1", "T2=2"],
+	}
+
+become
+
+	[ 
+		"X-Test", "AAA",
+		"Set-Cookie", "T1=1",
+		"Set-Cookie", "T2=2",
+	]
+
+=cut
 sub to_arrayref {
 	my ($headers) = @_;
 	my @res;
--- lib/Jedi/Helpers/Scalar.pm
+++ lib/Jedi/Helpers/Scalar.pm
@@ -1,9 +1,22 @@
 package Jedi::Helpers::Scalar;
+
+# ABSTRACT: Jedi Helpers for Scalar
+
 use strict;
 use warnings;
+
+# VERSION
+
 use Import::Into;
 use Module::Runtime qw/use_module/;
 
+=method import
+
+Equivalent in your module to :
+
+  use autobox SCALAR => Jedi::Helpers::Scalar
+
+=cut
 sub import {
 	my ($class) = @_;
 	my $target = caller;
@@ -11,17 +24,39 @@ sub import {
 	return;
 }
 
+=method full_path
+
+Add a trailing "/" to your path :
+
+	"/env"->full_path # /env/
+
+=cut	
 sub full_path {
 	my ($path) = @_;
 	$path .= '/' if substr($path, -1) ne '/';
 	return $path;
 }
 
+=method start_with
+
+Check if a path start with the value in param :
+
+	"/env/test"->start_with("/env") # true
+
+=cut	
 sub start_with {
 	my ($path, $start) = @_;
 	return substr($path, 0, length($start)) eq $start;
 }
 
+=method without_base
+
+Remove from the path, the base pass in params :
+
+	"/env/test"->without_base("/env") # /test/
+	"/env/test"->without_base("/env") # /test/
+
+=cut
 sub without_base {
 	my ($path, $base) = @_;
 
